dda46ba638eae6857cc6d9524709e51b9d345b1e
Rodion Kovalenko <30922133+RodionKovalenko@users.noreply.github.com>
2018-03-16 01:13:13 +0100

    Update RBFN.java

--- src/RBFN.java
+++ src/RBFN.java
@@ -22,7 +22,7 @@ public class RBFN {
 
     static double[][] weightMatrix;
     static double[] centers;
-    static double sigma = 1;
+    static double sigma = 0.00008;
     static int numberOfHiddenNeurons = 0;
 
     public static void trainAndTest(String a, String t, boolean visualize, String[] args, int index) {
@@ -32,10 +32,16 @@ public class RBFN {
     }
 
     public static void trainAndTest(double[][] a, double[][] t, boolean visualize, String[] args, int index) {
+        double[][] trainMenge = Statistics.divideTrainSet(a);
+        numberOfHiddenNeurons = trainMenge[0].length;
+        double[][] trainPreis = Statistics.divideTrainSet(t);
 
-        centers = new double[a[0].length];
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
+        double[][] testMenge = Statistics.divideTestSet(a);
+        double[][] testPreis = Statistics.divideTestSet(t);
+
+        centers = new double[trainMenge[0].length];
+        for (int i = 0; i < trainMenge.length; i++) {
+            for (int j = 0; j < trainMenge[i].length; j++) {
                 centers[j] = a[i][j];
             }
         }
@@ -43,13 +49,9 @@ public class RBFN {
 
         NetworkFunctions.printArray(centers);
 
-        double[][] trainMenge = Statistics.divideTrainSet(a);
 
-        numberOfHiddenNeurons = trainMenge[0].length;
-        double[][] trainPreis = Statistics.divideTrainSet(t);
 
-        double[][] testMenge = Statistics.divideTestSet(a);
-        double[][] testPreis = Statistics.divideTestSet(t);
+
 
 
         System.out.println("Interpolation matrix is being calculated");
@@ -72,7 +74,7 @@ public class RBFN {
         double[][] result = NetworkFunctions.matrixMultiply(
                 NetworkFunctions.transpose(weightMatrix), NetworkFunctions.transpose(m));
 
-        System.out.println("Accuracy train:" + Statistics.measureAccuracy(result, trainPreis));
+        System.out.println("Accuracy train:" + Statistics.measureAccuracy2(result, trainPreis));
         NetworkFunctions.printArray(result);
 
 
@@ -151,14 +153,31 @@ public class RBFN {
 
         double[] s = NetworkFunctions.convertToOneDimension(a);
         double[][] matrixS = new double[a[0].length][numberOfHiddenNeurons];
-        for (int i = 0; i < a[0].length; i++) {
-            for (int j = 0; j < numberOfHiddenNeurons; j++) {
-                matrixS[i][j] = calculateKernel(s[i], centers[j]);
 
-            }
+//        for (int i = 0; i < s.length; i++) {
+//            for (int k = 0; k < centers.length; k++) {
+//                matrixS[i][k] = calculateKernel( s[i], centers[k]);
+//
+//            }
+//        }
+
+        for (int i = 0; i < s.length; i++) {
+                matrixS[i]=subMatrix(matrixS[i],s,i);
         }
+        //NetworkFunctions.printArray(matrixS);
 
         return matrixS;
+    }
+
+    public static double[] subMatrix(double[] a, double[] s, int index) {
+            for (int k = 0; k < a.length; k++){
+                for(int j=0; j<s.length; j++)
+                    a[index] = calculateKernel(s[j], centers[k]);
+        }
+
+
+
+        return a;
     }
 
 
@@ -169,16 +188,16 @@ public class RBFN {
 
     public static void main(String[] args) {
 
-        double[][] input = new double[][]{NetworkFunctions.convertStringToNumbers(Data.mengen[15])};
-        double[][] target = new double[][]{NetworkFunctions.convertStringToNumbers(Data.preise[15])};
+        double[][] input = new double[][]{NetworkFunctions.convertStringToNumbers(Data.mengen[6])};
+        double[][] target = new double[][]{NetworkFunctions.convertStringToNumbers(Data.preise[6])};
 
-        double[][] menge = {{ 2,4,6,8,10,19, 40, 14, 20, 12, 19, 6}};
-        double[][] preis = {{ 4, 7, 10, 20, 14, 34, 50, 65, 6, 1, 13, 8}};
+        double[][] menge = {{2, 4, 6, 8, 10, 19, 40, 14, 20, 12, 19, 6, 123,51,200}};
+        double[][] preis = {{4, 7, 10, 20, 14, 34, 50, 65, 6, 1, 13, 8, 15, 8, 12}};
 
 
-    //    trainAndTest(input, target, true, args, 1);
+       trainAndTest(input, target, true, args, 1);
 
-        trainAndTest(menge, preis, true, args, 1);
+        //   trainAndTest(menge, preis, true, args, 1);
     }
 
 
